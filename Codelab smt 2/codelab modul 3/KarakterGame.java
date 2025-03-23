cf37e8e65de82bfe17c1f7711aa5f326e32c5c0e
Zaskiaramadhan <ramadhanizaskia753@gmail.com>
2025-03-24 05:34:21 +0700

    Add files via upload

---
+++ Codelab smt 2/codelab modul 3/KarakterGame.java
@@ -0,0 +1,56 @@
+class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    // Constructor
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void serang(KarakterGame target) {
+        System.out.println(this.nama + " menyerang " + target.getNama() + "!");
+    }
+}
+
+class Pahlawan extends KarakterGame {
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(this.getNama() + " menyerang " + target.getNama() + " menggunakan Orbital Strike!");
+        target.setKesehatan(target.getKesehatan() - 20);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    @Override
+    public void serang(KarakterGame target) {
+        System.out.println(this.getNama() + " menyerang " + target.getNama() + " menggunakan Snake Bite!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
